de2954266a33104b872a38fa308eedc381a1714c
RobzLegz <robzlegz@gmail.com>
2023-10-21 17:26:00 +0300

    f

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const Footer = () => {
-  return <footer className="p-4 bg-black w-full z-10 text-white border-t-2">LatSat © {new Date().getFullYear()}</footer>;
+  return (
+    <footer className="p-4 bg-black w-full z-10 text-white border-t-2">
+      LatSat © {new Date().getFullYear()}
+    </footer>
+  );
 };
 
 export default Footer;
