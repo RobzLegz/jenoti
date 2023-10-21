082f237f1e86863109a6710483e33629b742ea64
RobzLegz <robzlegz@gmail.com>
2023-10-21 17:01:26 +0300

    data

--- frontend/src/components/Description.tsx
+++ frontend/src/components/Description.tsx
@@ -1,87 +1,113 @@
 import Image from "next/image";
-import React from "react";
+import React, { useState } from "react";
 
 const Description = () => {
+  const [activeItem, setActiveItem] = useState<{
+    title: string;
+    images: string[];
+    description: string;
+  } | null>(null);
+
   return (
     <section
       className="w-full p-20 flex flex-col gap-36 z-20 bg-black/80"
       id="learn-more"
     >
-      <div className="flex w-full items-center justify-center gap-8">
-        <Image
-          src="/resources/communication.jpg"
-          alt="3D satelite position"
-          width={400}
-          height={400}
-          className="w-[400px] rounded-lg"
-        />
+      {activeItem ? <div className="w-full h-screen absolute top-0 left-0"></div> : null}
 
-        <strong className="text-white text-4xl max-w-[500px]">
-          24/7 data collection & comunication
-        </strong>
-      </div>
+      {data.map((item, i) => (
+        <div className="flex w-full items-center justify-center gap-8" key={i}>
+          {i % 2 === 0 ? (
+            item.images.length === 1 ? (
+              <Image
+                src={item.images[0]}
+                alt={item.title}
+                width={400}
+                height={400}
+                className="w-[400px] rounded-lg"
+              />
+            ) : item.images.length > 1 ? (
+              <div className="grid grid-cols-2 gap-2">
+                {item.images.map((img, i) => (
+                  <Image
+                    src={img}
+                    alt={item.title}
+                    width={400}
+                    height={400}
+                    className="w-[200px] h-[200px] object-cover rounded-lg"
+                  />
+                ))}
+              </div>
+            ) : null
+          ) : null}
 
-      <div className="flex w-full items-center justify-center gap-8">
-        <strong className="text-white text-4xl max-w-[500px]">
-          Real-time 3D satelite geopositioning
-        </strong>
+          <div className="flex flex-col">
+            <strong className="text-white text-4xl max-w-[500px]">
+              {item.title}
+            </strong>
 
-        <Image
-          src="/resources/3d-satelite-positioning.png"
-          alt="3D satelite position"
-          width={400}
-          height={400}
-          className="w-[400px] rounded-lg"
-        />
-      </div>
+            <button
+              className="w-48 h-12 rounded-full bg-black text-white border-2 border-white mt-6"
+              onClick={() => setActiveItem(item)}
+            >
+              Learn more
+            </button>
+          </div>
 
-      <div className="flex w-full items-center justify-center gap-8">
-        <Image
-          src="/resources/independence.jpg"
-          alt="3D satelite position"
-          width={400}
-          height={400}
-          className="w-[400px] rounded-lg"
-        />
-
-        <strong className="text-white text-4xl max-w-[500px]">
-          Independent from other networks and entities
-        </strong>
-      </div>
-
-      <div className="flex w-full items-center justify-center gap-8">
-        <strong className="text-white text-4xl max-w-[500px]">
-          Self-deorbiting mechanism
-        </strong>
-
-        <div className="grid grid-cols-2 gap-2">
-          <Image
-            src="/resources/deorbiting.jpg"
-            alt="3D satelite position"
-            width={400}
-            height={400}
-            className="w-[200px] h-[200px] object-cover rounded-lg"
-          />
-
-          <Image
-            src="/resources/deorbiting-1.jpg"
-            alt="3D satelite position"
-            width={400}
-            height={400}
-            className="w-[200px] h-[200px] object-cover rounded-lg"
-          />
-
-          <Image
-            src="/resources/deorbiting.gif"
-            alt="3D satelite position"
-            width={400}
-            height={400}
-            className="w-[200px] h-[200px] object-cover rounded-lg"
-          />
+          {i % 2 !== 0 ? (
+            item.images.length === 1 ? (
+              <Image
+                src={item.images[0]}
+                alt={item.title}
+                width={400}
+                height={400}
+                className="w-[400px] rounded-lg"
+              />
+            ) : item.images.length > 1 ? (
+              <div className="grid grid-cols-2 gap-2">
+                {item.images.map((img, i) => (
+                  <Image
+                    src={img}
+                    alt={item.title}
+                    width={400}
+                    height={400}
+                    className="w-[200px] h-[200px] object-cover rounded-lg"
+                  />
+                ))}
+              </div>
+            ) : null
+          ) : null}
         </div>
-      </div>
+      ))}
     </section>
   );
 };
 
 export default Description;
+
+const data = [
+  {
+    title: "24/7 data collection & comunication",
+    images: ["/resources/communication.jpg"],
+    description: "",
+  },
+  {
+    title: "Real-time 3D satelite geopositioning",
+    images: ["/resources/3d-satelite-positioning.png"],
+    description: "",
+  },
+  {
+    title: "Independent from other networks and entities",
+    images: ["/resources/independence.jpg"],
+    description: "",
+  },
+  {
+    title: "Self-deorbiting mechanism",
+    images: [
+      "/resources/deorbiting.jpg",
+      "/resources/deorbiting-1.jpg",
+      "/resources/deorbiting.gif",
+    ],
+    description: "",
+  },
+];
--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -7,18 +7,23 @@ const Header = () => {
       <Test />
 
       <div className="w-full h-full absolute top-0 left-0 z-10 bg-black/30 flex flex-col items-center justify-center">
-        <h1 className="text-white text-center max-w-[1000px] leading-[65px]">
-          LatSat - private independent satelite network
-        </h1>
+        <h1 className="text-white text-center mb-4">LatSat</h1>
 
-        <div className="flex items-center justify-center gap-4 mt-8">
+        <h2 className="text-white text-center max-w-[1000px] leading-[65px] opacity-80">
+          Private independent satelite network
+        </h2>
+
+        <div className="flex items-center justify-center gap-4 mt-10">
           <Link
             href="#learn-more"
             className="flex items-center justify-center w-60 h-16 border-2 border-white text-white bg-black text-xl rounded-full"
           >
             Learn more
           </Link>
-          <button className="w-60 h-16 bg-white text-black text-xl rounded-full">
+          <button
+            className="w-60 h-16 bg-white text-black text-xl rounded-full opacity-50"
+            disabled
+          >
             Buy now
           </button>
         </div>
